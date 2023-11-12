0569cfa10efef9e3f1f6bc4d45bd8db3ef4beade
Daanieeel <d4niel.dd060306@gmail.com>
2023-11-12 22:32:54 +0100

    new account setting: showTyping

--- httpServer/models/user/AccountSettings.js
+++ httpServer/models/user/AccountSettings.js
@@ -2,5 +2,9 @@ export default {
     loginCookies: {
         name: "loginCookies",
         defaultValue: "false"
+    },
+    showTyping: {
+        name: "showTyping",
+        defaultValue: "true"
     }
 }
