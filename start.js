447e4ec1ef52b5f6c49576e7a2f160e06692a9c5
Daanieeel <d4niel.dd060306@gmail.com>
2023-12-07 00:20:58 +0100

    new start script test

--- start.js
+++ start.js
@@ -1,1 +1,26 @@
-// start indexStart.js and socketStart.js with node
+// Import the child_process module
+import { spawn } from  'child_process';
+
+// Spawn a new child process for each script
+const socketStart = spawn('node', ['./socketStart.js']);
+const indexStart = spawn('node', ['./indexStart.js']);
+
+// Log any output from the socketStart script
+socketStart.stdout.on('data', (data) => {
+  console.log(`socketStart output: ${data}`);
+});
+
+// Log any errors from the socketStart script
+socketStart.stderr.on('data', (data) => {
+  console.error(`socketStart error: ${data}`);
+});
+
+// Log any output from the indexStart script
+indexStart.stdout.on('data', (data) => {
+  console.log(`indexStart output: ${data}`);
+});
+
+// Log any errors from the indexStart script
+indexStart.stderr.on('data', (data) => {
+  console.error(`indexStart error: ${data}`);
+});
