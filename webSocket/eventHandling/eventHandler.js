21e09588df708be2468150917004519286b410df
Daanieeel <d4niel.dd060306@gmail.com>
2023-12-10 00:41:54 +0100

    Update eventHandler.js

--- webSocket/eventHandling/eventHandler.js
+++ webSocket/eventHandling/eventHandler.js
@@ -7,13 +7,14 @@
 
 import logger from "../../tools/logging/logger.js";
 import errors from "../../errors.js";
+import socketEvents from "./socketEvents.js";
 
 /**
  * @description This function handles different types of events that can occur in a WebSocket connection.
  * @param {Object} ws - The WebSocket connection object.
  * @param {Object} data - The data received from the WebSocket connection.
  * @param {String} messageId - The id of the socket messages.
- * @param {Number} date - The date when the messages was received.
+ * @param {Number} date - The date when the message was received.
  */
 export function handleEvents(ws, data, messageId, date) {
 
@@ -22,8 +23,15 @@ export function handleEvents(ws, data, messageId, date) {
     const event = data.event;
     const payload = data.payload;
 
+    for(const socketEvent in socketEvents){
 
-    logger.socket.debug(`Message #${messageId} processed in ${now - date} ms`)
+        if(socketEvent === event){
+            socketEvents[event].handler(ws, data, messageId, date);
+            logger.socket.debug(`Message #${messageId} processed in ${now - date} ms`)
+            break;
+        }
+
+    }
 
     logger.socket.debug(`Message #${messageId} contains unknown event ${event}`);
     ws.send(
