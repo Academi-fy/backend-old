c0a1dd72a7a38b8308f17782a2f55785afc59a7a
Daanieeel <d4niel.dd060306@gmail.com>
2023-12-10 01:04:01 +0100

    socket event doc #2

--- webSocket/eventHandling/handlers/users/UserCourseRemoveEvent.js
+++ webSocket/eventHandling/handlers/users/UserCourseRemoveEvent.js
@@ -3,6 +3,16 @@
  * @author Daniel Dopatka
  * @copyright 2023 Daniel Dopatka, Linus Bung
  */
+import logger from "../../../../tools/logging/logger.js";
+
+/**
+ * @description Function handling the UserCourseRemoveEvent.
+ * @param {Object} ws - The WebSocket connection object.
+ * @param {Object} data - The data received from the WebSocket connection.
+ * @param {String} messageId - The id of the socket message.
+ * @param {Number} date - The date when the event was received.
+ */
 export default function (ws, data, messageId, date) {
 
+    logger.socket.debug(`Message #${messageId} processed in ${Date.now() - date} ms`)
 }
